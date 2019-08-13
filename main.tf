3b6106aa2cdeeb9a91b1a95467b92f5fde08c355
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-08-13 15:56:39 +0200

    Fix for name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "acr" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "${var.name}registry"
+  name                     = format("%sregistry", lower(replace(var.name, "/[[:^alnum:]]/", "")))
   resource_group_name      = azurerm_resource_group.acr.name
   location                 = azurerm_resource_group.acr.location
   sku                      = var.sku
